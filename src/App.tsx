78cf54bfac61ce08d27d6a8ad9cdc06f0be3bf76
PaglaDasu <snehasishbabin2001@gmail.com>
2024-03-19 02:32:44 +0530

    Custom Biolerplate TSX

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center text-center p-8 text-xl">
+        Welcome to the template of PaglaDasu.
+      </div>
+    </>
+  );
+}
+
+export default App;
